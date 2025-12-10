98c329b720bc07296c32711af4231f8d44116c91
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-10 14:32:46 +0200

    init

---
+++ MysticZen/App/MysticZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysticZenApp.swift
+//  MysticZen
+//
+//  Created by Serhii Babchuk on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MysticZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppEntryPoint()
+        }
+    }
+}
